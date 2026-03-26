6e892c0dd05dd6cd8c902f20eb06102698838a12
JustoBellingi <jbellingi2006@gmail.com>
2026-03-26 15:50:59 -0300

    actualizar.2

--- src/pages/Contacto.tsx
+++ src/pages/Contacto.tsx
@@ -46,13 +46,7 @@ export default function Contacto() {
         </div>
 
         <div className="contacto-mapa">
-          <iframe 
-            src="https://www.google.com/maps/embed?pb=!1m18..."
-            width="100%"
-            height="350"
-            style={{ border: 0, borderRadius: "12px" }}
-            loading="lazy"
-          ></iframe>
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d422.2309767960373!2d-58.054732426042804!3d-34.879290976892705!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95a2dc2a97f215a7%3A0x9d708899addcca9f!2sCentro%20de%20terapias%20para%20el%20desarrollo!5e0!3m2!1ses!2sar!4v1774550859562!5m2!1ses!2sar" width="600" height="450" style={{ border: 0 }} allowFullScreen loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
         </div>
 
       </div>
