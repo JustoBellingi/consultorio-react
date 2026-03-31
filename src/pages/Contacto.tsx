7f770ba054df4a2cfcfa194bc09cac3ae7dd327d
JustoBellingi <jbellingi2006@gmail.com>
2026-03-31 12:30:05 -0300

    mejoras para la pagina en el celular.3

--- src/pages/Contacto.tsx
+++ src/pages/Contacto.tsx
@@ -42,12 +42,7 @@ function Contacto() {
 
         {/* MAPA */}
         <div className="mapa">
-          <iframe
-            src="https://www.google.com/maps?q=City+Bell,+La+Plata&output=embed"
-            loading="lazy"
-            allowFullScreen
-            referrerPolicy="no-referrer-when-downgrade"
-          ></iframe>
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d422.2309767960373!2d-58.054732426042804!3d-34.879290976892705!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95a2dc2a97f215a7%3A0x9d708899addcca9f!2sCentro%20de%20terapias%20para%20el%20desarrollo!5e0!3m2!1ses!2sar!4v1774550859562!5m2!1ses!2sar" width="300" height="225" style={{ border: 2 }} allowFullScreen loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
         </div>
 
       </div>
